ae68e6bf5ca19f4efb8c7401ae95c804154b02df
VadimSorokolit <macintosh@ukr.net>
2025-09-15 12:59:05 +0300

    Init project

---
+++ NumericFacts/NumericFacts/AppMain/NumericFactsApp.swift
@@ -0,0 +1,33 @@
+//
+//  NumericFactsApp.swift
+//  NumericFacts
+//
+//  Created by Vadim Sorokolit on 15.09.2025.
+//
+    
+import SwiftUI
+import SwiftData
+
+@main
+struct NumericFactsApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+        
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+    
+}
